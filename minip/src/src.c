f17311685adaf79ea0ff37c9ec25c65ab95d9675
99002661 <71481321+99002661@users.noreply.github.com>
2020-09-22 19:10:28 +0530

    Update src.c

--- minip/src/src.c
+++ minip/src/src.c
@@ -1,1 +1,13 @@
 
+#include <calci.h>
+
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
