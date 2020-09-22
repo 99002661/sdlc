e24fe18f36506a52a5b4eb18d5392bc51c43c660
99002661 <71481321+99002661@users.noreply.github.com>
2020-09-22 19:09:31 +0530

    Update calci.h

--- minip/inc/calci.h
+++ minip/inc/calci.h
@@ -1,1 +1,17 @@
 
+#ifndef __CALCI_H__
+#define __CALCI_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+int add(int operand1, int operand2);
+
+
+int subtract(int operand1, int operand2);
+
+
+
+
+#endif  
